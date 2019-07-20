b2abe6f37dfaad57bfd4cc194db22c53b491aecd
jleapeMIT <jleape@mit.edu>
2019-07-19 22:31:38 -0300

    router arg

---
+++ test.py
@@ -0,0 +1,6 @@
+import argparse
+import datetime
+#!/Users/jonathanleape/jython2.7.0/bin/jython
+from org.opentripplanner.scripting.api import OtpsEntryPoint
+now = datetime.datetime.now()
+print("hey")
